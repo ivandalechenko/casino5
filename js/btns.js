18587f8ab737eaacf853df4c1b230a52d6a827c2
ivandalechenko <ivandalechenko@gmail.com>
2023-05-04 17:10:16 +0300

    meow

--- js/btns.js
+++ js/btns.js
@@ -1,57 +1,61 @@
-document.getElementById('login_btn').onclick = function () {
-    document.getElementById('login_form').classList.remove('dnone');
+document.getElementById('enter_btn').onclick = function () {
+    document.getElementById('enter_form').classList.toggle('dnone');
 }
-document.getElementById('registration_cross').onclick = function () {
-    document.getElementById('registration_form').classList.add('dnone');
-}
-document.getElementById('login_cross').onclick = function () {
-    document.getElementById('login_form').classList.add('dnone');
-}
-document.getElementById('registration_btn').onclick = function () {
-    document.getElementById('registration_form').classList.remove('dnone');
-}
+
+// document.getElementById('login_btn').onclick = function () {
+//     document.getElementById('login_form').classList.remove('dnone');
+// }
+// document.getElementById('registration_cross').onclick = function () {
+//     document.getElementById('registration_form').classList.add('dnone');
+// }
+// document.getElementById('login_cross').onclick = function () {
+//     document.getElementById('login_form').classList.add('dnone');
+// }
+// document.getElementById('registration_btn').onclick = function () {
+//     document.getElementById('registration_form').classList.remove('dnone');
+// }
 
 
 
 
-document.getElementById('game_cross').onclick = function () {
-    document.getElementById('game_popup').classList.add('dnone');
-    document.getElementById('iframe').src = "";
-}
-document.getElementById('bj').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/BlackJack21/html5-files/index.html";
-    document.getElementById('game_name').innerHTML = 'Black Jack';
-}
-document.getElementById('poker').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/Candy Poker/index.html";
-    document.getElementById('game_name').innerHTML = 'CANDY POCKER';
-}
-document.getElementById('pirates').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/Caribbean Pirates/game/index.html";
-    document.getElementById('game_name').innerHTML = 'CARIBBEAN PIRATES';
-}
-document.getElementById('spin').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/game3/Html5 files/index.html";
-    document.getElementById('game_name').innerHTML = 'SPIN THE WHEEL';
-}
-document.getElementById('africaking').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/game1/index.html";
-    document.getElementById('game_name').innerHTML = 'KING OF AFRICA';
-}
-document.getElementById('treasure').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/game2/html5/index.html";
-    document.getElementById('game_name').innerHTML = 'BOOK OF TREASURES 2';
-}
-document.getElementById('alien').onclick = function () {
-    document.getElementById('game_popup').classList.remove('dnone');
-    document.getElementById('iframe').src = "./games/Outerspace/game/index.html";
-    document.getElementById('game_name').innerHTML = 'OUTERSPACE';
-}
+// document.getElementById('game_cross').onclick = function () {
+//     document.getElementById('game_popup').classList.add('dnone');
+//     document.getElementById('iframe').src = "";
+// }
+// document.getElementById('bj').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/BlackJack21/html5-files/index.html";
+//     document.getElementById('game_name').innerHTML = 'Black Jack';
+// }
+// document.getElementById('poker').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/Candy Poker/index.html";
+//     document.getElementById('game_name').innerHTML = 'CANDY POCKER';
+// }
+// document.getElementById('pirates').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/Caribbean Pirates/game/index.html";
+//     document.getElementById('game_name').innerHTML = 'CARIBBEAN PIRATES';
+// }
+// document.getElementById('spin').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/game3/Html5 files/index.html";
+//     document.getElementById('game_name').innerHTML = 'SPIN THE WHEEL';
+// }
+// document.getElementById('africaking').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/game1/index.html";
+//     document.getElementById('game_name').innerHTML = 'KING OF AFRICA';
+// }
+// document.getElementById('treasure').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/game2/html5/index.html";
+//     document.getElementById('game_name').innerHTML = 'BOOK OF TREASURES 2';
+// }
+// document.getElementById('alien').onclick = function () {
+//     document.getElementById('game_popup').classList.remove('dnone');
+//     document.getElementById('iframe').src = "./games/Outerspace/game/index.html";
+//     document.getElementById('game_name').innerHTML = 'OUTERSPACE';
+// }
 
 
